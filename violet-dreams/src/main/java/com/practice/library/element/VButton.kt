ae898f2ad18acdd6bac20e7640f024bebb99b72b
Wooyoung Myung <mwy3055@gmail.com>
2022-03-05 21:16:28 +0900

    custom composable: Button

--- violet-dreams/src/main/java/com/practice/library/element/VButton.kt
+++ violet-dreams/src/main/java/com/practice/library/element/VButton.kt
@@ -1,47 +1,69 @@
 package com.practice.library.element
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.practice.library.shape.VBezierShape
 import com.practice.library.theme.VioletDreamsTheme
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun VButton(
-    text: String,
+    onClick: () -> Unit,
     modifier: Modifier = Modifier,
-    onClick: () -> Unit = {},
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+//    elevation: ButtonElevation? = ButtonDefaults.elevation(),
+    border: BorderStroke = BorderStroke(width = 1.dp, color = MaterialTheme.colors.primaryVariant),
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    content: @Composable RowScope.() -> Unit
 ) {
-    Text(
-        text = text,
-        style = TextStyle(
-            color = MaterialTheme.colors.onPrimary,
-            fontSize = 22.sp,
-            fontWeight = FontWeight.Normal,
-        ),
-        textAlign = TextAlign.Center,
-        modifier = modifier
-            .wrapContentSize()
-            .graphicsLayer {
-                shape = VBezierShape()
-                clip = true
+    val contentColor by colors.contentColor(enabled)
+    Surface(
+        modifier = modifier.graphicsLayer {
+            shape = VBezierShape()
+            clip = true
+        },
+        shape = VBezierShape(),
+        color = colors.backgroundColor(enabled = enabled).value,
+        contentColor = contentColor.copy(alpha = 1f),
+        border = border,
+//        elevation = elevation?.elevation(enabled, interactionSource)?.value ?: 0.dp,
+        onClick = onClick,
+        enabled = enabled,
+        role = Role.Button,
+        interactionSource = interactionSource,
+        indication = rememberRipple()
+    ) {
+        CompositionLocalProvider(LocalContentAlpha provides contentColor.alpha) {
+            ProvideTextStyle(value = MaterialTheme.typography.button) {
+                Row(
+                    modifier = Modifier
+                        .defaultMinSize(
+                            minWidth = ButtonDefaults.MinWidth,
+                            minHeight = ButtonDefaults.MinHeight
+                        )
+                        .padding(contentPadding),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                    content = content
+                )
             }
-            .background(color = MaterialTheme.colors.primary)
-            .clickable(onClick = onClick)
-            .padding(horizontal = 25.dp, vertical = 10.dp)
-    )
+        }
+    }
 }
 
 @Preview(showBackground = true)
@@ -49,9 +71,11 @@ fun VButton(
 private fun VButtonPreview() {
     VioletDreamsTheme {
         VButton(
-            text = "I want to go home",
+            onClick = {},
             modifier = Modifier.padding(10.dp)
-        )
+        ) {
+            Text("I want to go home")
+        }
     }
 }
 
@@ -60,8 +84,10 @@ private fun VButtonPreview() {
 private fun VButtonPreviewShortText() {
     VioletDreamsTheme {
         VButton(
-            text = "확인",
-            modifier = Modifier.padding(10.dp)
-        )
+            onClick = {},
+            modifier = Modifier.padding(10.dp),
+        ) {
+            Text("확인")
+        }
     }
 }
