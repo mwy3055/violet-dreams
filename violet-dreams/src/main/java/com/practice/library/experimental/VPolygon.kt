ab9b88c45539259abb5d08d3b1e02c7eabf18c37
Wooyoung Myung <mwy3055@gmail.com>
2022-03-09 19:12:26 +0900

    improve style and give more parameters to polygon

--- violet-dreams/src/main/java/com/practice/library/experimental/VPolygon.kt
+++ violet-dreams/src/main/java/com/practice/library/experimental/VPolygon.kt
@@ -5,8 +5,7 @@ import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.animate
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -18,25 +17,42 @@ import androidx.compose.ui.unit.dp
 import com.practice.library.shape.polygonOutlineLength
 import com.practice.library.shape.polygonPath
 import com.practice.library.theme.VioletDreamsTheme
+import kotlin.math.min
 
 @Composable
 fun VPolygon(
     sides: Int,
-    radius: Float,
     modifier: Modifier = Modifier,
-    dashPathEffect: DashPathEffect? = null,
+    lineWidth: Dp? = null,
+    interval: FloatArray? = null,
+    phase: Float? = null,
     polygonPadding: Dp = 0.dp,
 ) {
     val primaryColor = MaterialTheme.colors.primary.toArgb()
 
     Canvas(
         modifier = modifier
-            .size(radius.dp)
-            .padding(polygonPadding),
+            .padding(polygonPadding)
+            .wrapContentSize()
+            .aspectRatio(1f),
         onDraw = {
+            val radius = min(size.width, size.height) / 2
+
             val polygonPath = Path().polygonPath(sides, radius, size).asAndroidPath()
+            val outlineLength = polygonOutlineLength(sides, radius)
+
+            val dashPathEffect = if (!arrayOf(interval, phase).contains(null)) {
+                DashPathEffect(
+                    interval!!.map { it * outlineLength }.toFloatArray(),
+                    phase!!
+                )
+            } else {
+                null
+            }
+
             val paint = Paint().asFrameworkPaint().apply {
                 color = primaryColor
+                strokeWidth = lineWidth?.value ?: radius / 20f
                 style = android.graphics.Paint.Style.STROKE
                 pathEffect = dashPathEffect
             }
@@ -51,17 +67,12 @@ fun VPolygon(
 @Composable
 private fun VPolygonPreview() {
     val sides = 6
-    val radius = 100f
     val polygonPadding = 10.dp
 
     // Animate outline
     var ratio by remember { mutableStateOf(0f) }
 
-    val outlineLength = polygonOutlineLength(sides, radius)
-    val dashPathEffect = DashPathEffect(
-        floatArrayOf(outlineLength * ratio, outlineLength * (1 - ratio)),
-        0f
-    )
+    val interval = floatArrayOf(ratio, 1 - ratio)
 
     LaunchedEffect(key1 = true) {
         animate(
@@ -79,8 +90,8 @@ private fun VPolygonPreview() {
     VioletDreamsTheme {
         VPolygon(
             sides = sides,
-            radius = radius,
-            dashPathEffect = dashPathEffect,
+            modifier = Modifier.size(400.dp),
+            interval = interval,
             polygonPadding = polygonPadding
         )
     }
