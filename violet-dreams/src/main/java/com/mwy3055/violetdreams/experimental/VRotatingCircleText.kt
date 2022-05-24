87e573af33f2086574a3a2aa863105334dc7d7a3
Wooyoung Myung <mwy3055@gmail.com>
2022-05-24 22:13:55 +0900

    refactor: inserts a circle text on VRotatingPane, not directly rotating it

--- violet-dreams/src/main/java/com/mwy3055/violetdreams/experimental/VRotatingCircleText.kt
+++ violet-dreams/src/main/java/com/mwy3055/violetdreams/experimental/VRotatingCircleText.kt
@@ -1,13 +1,10 @@
 package com.mwy3055.violetdreams.experimental
 
-import androidx.compose.animation.core.*
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
@@ -21,27 +18,14 @@ fun VRotatingCircleText(
     modifier: Modifier = Modifier,
     fontSize: TextUnit = 60.sp,
     textColor: Color = MaterialTheme.colors.onPrimary,
-    durationMillis: Int = 2000,
-    easing: Easing = CubicBezierEasing(0.8f, 0.1f, 0.2f, 0.9f),
 ) {
-    val angleAnim = rememberInfiniteTransition()
-    val angle by angleAnim.animateFloat(
-        initialValue = 0f,
-        targetValue = 360f,
-        animationSpec = infiniteRepeatable(
-            tween(
-                durationMillis = durationMillis,
-                easing = easing,
-            )
+    VRotatingPane(modifier = modifier) {
+        VCircleText(
+            text = text,
+            fontSize = fontSize,
+            textColor = textColor
         )
-    )
-
-    VCircleText(
-        text = text,
-        modifier = modifier.rotate(angle),
-        fontSize = fontSize,
-        textColor = textColor
-    )
+    }
 }
 
 @Preview(showBackground = true)
